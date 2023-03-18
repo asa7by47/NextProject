23b8aedae1c638a17486b52d103e1e60644b048b
asa7by47 <omartamergado@gmail.com>
2023-03-18 13:46:56 +0200

    first commit

---
+++ src/Components/WhoWeAre/WhoWeAre.jsx
@@ -0,0 +1,53 @@
+import WeAre from "@/Components/WeAre/WeAre";
+import React, { useState } from "react";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
+import style from "./WhoWeAre.module.css";
+const WhoWeAre = () => {
+  const [counterOn, setCounterOn] = useState(false);
+  const conditionOne = counterOn && <CountUp start={0} end={25} delay={0} duration={2} />
+  const conditionTwo = counterOn && <CountUp start={0} end={12} delay={0} duration={2} />
+  const conditionThree = counterOn && <CountUp start={0} end={7} delay={0} duration={1} />
+  const conditionFour = counterOn && <CountUp start={0} end={8} delay={0} duration={1} />
+  return (
+    <>
+      <ScrollTrigger
+        onEnter={() => setCounterOn(true)}
+        onExit={() => setCounterOn(false)}
+      >
+        <section className={`p-5 ${style.bgImage}`}>
+          <div className="text-center text-white">
+            <h4 className="">WHO WE ARE </h4>
+          </div>
+          <div className="container">
+            <div className="row">
+              <WeAre
+                title="Land Area Of projects"
+                number={conditionOne}
+                plus={style.plus}
+              />
+              <WeAre
+                title="BUE"
+                number={conditionTwo}
+                plus={style.plus}
+              />
+              <WeAre
+                title="Year Of experience"
+                number={conditionThree}
+                plus={style.plus}
+              />
+              <WeAre
+                title="Projects Completed"
+                number={conditionFour}
+                plus={style.plus}
+              />
+
+            </div>
+          </div>
+        </section>
+      </ScrollTrigger>
+    </>
+  );
+};
+
+export default WhoWeAre;
